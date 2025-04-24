a881e3ddc786a3cb78b4d19dde5b8c290d50c1f6
subtosolo <elliot.0523.r@gmail.com>
2025-04-24 16:45:28 -0400

    Update particles.js

--- particles.js
+++ particles.js
@@ -1,9 +1,5 @@
 /* -----------------------------------------------
-/* Author : Vincent Garreau  - vincentgarreau.com
-/* MIT license: http://opensource.org/licenses/MIT
-/* Demo / Generator : vincentgarreau.com/particles.js
-/* GitHub : github.com/VincentGarreau/particles.js
-/* How to use? : Check the GitHub README
+/* Author : Roman Easton  - soloo.fun
 /* v2.0.0
 /* ----------------------------------------------- */
 
